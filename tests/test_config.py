78d6b775fa738bdff3746000c1496bc7546e9f14
Lokesh Naga Sai Tungala <lokeshnagasaitungala79@gmail.com>
2026-09-04 10:00:12 +0530

    Creating Test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,17 +1,70 @@
 import pytest
-class NotInRange(Exception):
-    def __init__(self , message = "value not in range"):
-        self.message = message
-        super().__init__(self.message)
+import json
+import logging
+import os
 
-def test_generic():
-    a = 2
-    with pytest.raises(NotInRange):
-        if a not in range(10 , 20):
-            raise NotInRange
+from prediction_service.prediction import form_response, api_response
+import prediction_service
 
-def test_something():
-    a = 2
-    with pytest.raises(NotInRange):
-        if a not in range(10 , 20):
-            raise NotInRange
+
+input_data = {
+    "Incorrect Range": {
+        "fixed_acidity": 7897987,
+        "volatile_acidity": 555,
+        "citric_acid": 99,
+        "residual_sugar": 99,
+        "chlorides": 12,
+        "free_sulphur_dioxide": 789,
+        "total_sulfur_dioxide": 75,
+        "density": 2,
+        "pH": 3,
+        "sulphates": 9,
+        "alcohol": 9
+    },
+
+    "correct Range": {
+    "fixed_acidity": 5,
+    "volatile_acidity": 1,
+    "citric_acid": 0.5,
+    "residual_sugar": 10,
+    "chlorides": 0.5,
+    "free_sulfur_dioxide": 3,
+    "total_sulfur_dioxide": 75,
+    "density": 1,
+    "pH": 3,
+    "sulphates": 1,
+    "alcohol": 9
+},
+
+    "Incorrect col": {
+        "fixed_acidity": 5,
+        "volatile_acidity": 1,
+        "citric_acid": 0.5,
+        "residual_sugar": 10,
+        "chlorides": 0.5,
+        "free_sulfur_dioxide": 3,
+        "total_sulfur_dioxide": 75,
+        "density": 1,
+        "pH": 3,
+        "sulphates": 1,
+        "alcohol": 9
+    }
+}
+
+
+TARGET_range = {
+    "min": 3.0,
+    "max": 8.0
+}
+
+
+def test_form_response_correct_range():
+    data = input_data["correct Range"]
+
+    res = form_response(data)
+
+    assert TARGET_range["min"] <= res <= TARGET_range["max"]
+
+def test_api_response_correct_range(data = input_data["correct Range"]):
+    res = api_response(data)
+    assert TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
